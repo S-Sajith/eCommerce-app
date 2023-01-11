28ae432c241a426e6877481a166d3eb32bdf6e52
Sajith S <88919814+S-Sajith@users.noreply.github.com>
2023-01-11 11:04:46 +0000

    Add files via upload

---
+++ 03-frontend/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc:{
+        clientId:'0oa7vx3u73RYxImiW5d7',
+        issuer:'https://dev-63175198.okta.com/oauth2/default',
+        redirectUri: 'https://localhost:4200/login/callback',
+        scopes: ['openid','profile','email']
+    }
+
+}
